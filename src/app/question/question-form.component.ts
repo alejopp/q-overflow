41e61e6a0af296c5f7934fe50592a4fa53686555
alejopp <alejo51120@gmail.com>
2022-02-06 20:41:30 -0500

    icons to question-form.component added

--- src/app/question/question-form.component.ts
+++ src/app/question/question-form.component.ts
@@ -1,14 +1,34 @@
 import { Question } from './question.model';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import icons from './icons';
 
 
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html',
+   styles: [`
+    i { font-size: 3rem }
+    small{display: block}
+   `]
 })
 export class QuestionFormComponent implements OnInit {
 
+  icons: Array<any> = icons;
+
+  getIconVersion(icon: any){
+    let version;
+    if(icon.versions.font.includes('plain-wordmark')){
+        version = 'plain-wordmark'
+    }else{
+        version = icon.versions.font[0];
+    }
+
+    if(icon.name=='illustrator'){
+        version = icon.versions.svg[0];
+    }
+    return version;
+  }
   constructor() { }
 
   ngOnInit(): void {
@@ -19,7 +39,7 @@ export class QuestionFormComponent implements OnInit {
       form.value.title,
       form.value.description,
       new Date(),
-      'none'
+      form.value.icon
     )
     console.log(q);
   }
