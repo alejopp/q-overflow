5376193132d14c64bb1352f994aaaa326856c122
alejopp <alejo51120@gmail.com>
2022-02-09 00:53:47 -0500

    configuring express and defining the endpoint of questions. Some extra configurations added to the file package.json

--- server/index.js
+++ server/index.js
@@ -1,16 +1,12 @@
 import  http from 'http'
 import Debug from 'debug'
+import app from './app'
 
 const PORT = 3001
 const debug = new Debug('q-overflow:root')
 
-const app = http.createServer((req, res) => {
-  debug('New request')
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('Hola desde Q-Overflow')
-  res.end()
-})
+
 
 app.listen(PORT, () => {
   debug(`Server running at port ${PORT}`)
-})
+});
